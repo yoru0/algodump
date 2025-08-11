9fe2700ac57b68d6efb9049f41806627b5f58107
yoru <jordanchandra1@gmail.com>
2025-08-11 09:05:01 +0700

    2094A

---
+++ dump/2094A.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int t; cin >> t;
+    while(t--) {
+        string a, b, c; cin >> a >> b >> c;
+        cout << a[0] << b[0] << c[0] << endl;
+    }
+
+    return 0;
+}
