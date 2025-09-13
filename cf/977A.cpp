f05187ff86eeba81b3b081d673f93f0e8142b63d
yoru <jordanchandra1@gmail.com>
2025-09-13 23:10:08 +0700

    cf

---
+++ cf/977A.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int a, b; cin >> a >> b;
+	for(int i = 0; i < b; i++){
+		if(a % 10 == 0)
+			a /= 10;
+		else
+			a -= 1;
+	}
+	
+	cout << a << endl;
+	
+	return 0;
+}
