d44b0005d443e7c6ada30576ffebf2a8fd272963
yoru <jordanchandra1@gmail.com>
2025-09-26 22:14:46 +0700

    .

---
+++ lc/203.cpp
@@ -0,0 +1,41 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
+class Solution {
+public:
+    ListNode *removeElements(ListNode *head, int val) {
+        ListNode dummy(0, head);
+        ListNode *prev = &dummy;
+        ListNode *cur = head;
+
+        while (cur != nullptr) {
+            if (cur->val == val) {
+                prev->next = cur->next;
+                cur = cur->next;
+            } else {
+                prev = cur;
+                cur = cur->next;
+            }
+        }
+
+        return dummy.next;
+    }
+};
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    Solution sol;
+    
+
+    return 0;
+}
