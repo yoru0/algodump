b26833ce985aeb98bd28db57e7152c6cd6cdc493
yoru <jordanchandra1@gmail.com>
2025-10-07 22:28:20 +0700

    .

---
+++ lc/9.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    bool isPalindrome(int x) {
+        string y = to_string(x);
+
+        int i = 0, j = y.size() - 1;
+        while (i < j) {
+            if (y[i] != y[j])
+                return false;
+            i++;
+            j--;
+        }
+
+        return true;
+    }
+};
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    Solution sol;
+    cout << sol.isPalindrome(121) << endl;
+
+    return 0;
+}
